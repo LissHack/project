d29a3a910bc1df4c6d3483ec294ccfcbc2bdd3b3
Liss <les.karo@yandex.ru>
2023-05-20 22:00:09 +0400

    Работа со списками

--- src/App.js
+++ src/App.js
@@ -2,23 +2,17 @@ import React, {useState} from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import './styles/App.css'
+import PostItem from "./components/PostItem";
 
 function App() {
     const [value, setValue] = useState('Text in input')
 
     return (
         <div className="App">
-            <div className='post'>
-                <div className="post__content">
-                    <strong>1. Java Script</strong>
-                    <div>
-                        Javascript - язык программирования
-                    </div>
-                </div>
-                <div className="post__btns">
-                    <button>Удалить</button>
-                </div>
-            </div>
+            <PostItem post={{id:1, title: 'Javascript', body:'Description'}}/>
+            <PostItem post={{id:1, title: 'Javascript', body:'Description'}}/>
+            <PostItem post={{id:1, title: 'Javascript', body:'Description'}}/>
+
         </div>
     );
 }
