5e6f4c75b2427f4bf3a238ff33704a584c1746df
Liss <les.karo@yandex.ru>
2023-05-20 22:12:33 +0400

    Статичный список

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import './styles/App.css'
 import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 
 function App() {
-    const [value, setValue] = useState('Text in input')
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'Javascript', body: 'Description'},
+        {id: 2, title: 'Python', body: 'Description'},
+        {id: 3, title: 'Java', body: 'Description'},
+
+    ])
 
     return (
         <div className="App">
-            <PostItem post={{id:1, title: 'Javascript', body:'Description'}}/>
-            <PostItem post={{id:1, title: 'Javascript', body:'Description'}}/>
-            <PostItem post={{id:1, title: 'Javascript', body:'Description'}}/>
-
+            <PostList posts={posts} title='Список постов'/>
         </div>
     );
 }
