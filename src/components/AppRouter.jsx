a281491dc6bc6b371635cbf6a0c58222df932854
Liss <les.karo@yandex.ru>
2023-06-07 16:01:40 +0400

    useContext. Глобальные данные, авторизация

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Navigate, Route, Routes} from "react-router-dom";
-
 import {publicRoutes, privateRoutes} from "../router/Router";
+import {AuthContext} from "../context";
 
 const AppRouter = () => {
-    const isAuth = false;
+    const {isAuth, setIsAuth} = useContext(AuthContext)
+
     return (
         isAuth
             ? <Routes>
                 {privateRoutes.map(route =>
                     <Route
+                        key={route.path}
                         path={route.path}
                         element={<route.component/>}
                         exact={route.exact}
@@ -24,6 +26,7 @@ const AppRouter = () => {
             <Routes>
                 {publicRoutes.map(route =>
                     <Route
+                        key={route.path}
                         path={route.path}
                         element={<route.component/>}
                         exact={route.exact}
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import MyInput from "../components/UI/input/MyInput";
 import MyButton from "../components/UI/button/MyButton";
+import {AuthContext} from "../context";
 
 const Login = () => {
+    const {isAuth, setIsAuth} = useContext(AuthContext)
+    const login = e => {
+        e.preventDefault();
+        setIsAuth(true);
+    }
+
     return (
         <div>
             <h2>Страница для логина</h2>
-            <form>
+            <form onSubmit={login}>
                 <MyInput type="text" placeholder='Введите логин'/>
                 <MyInput type="password" placeholder='Введите пароль'/>
                 <MyButton>Войти</MyButton>
