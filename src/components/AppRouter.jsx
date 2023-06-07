c25777c3a212002f32fb6facb0c90459eea8144d
Liss <les.karo@yandex.ru>
2023-06-07 16:45:14 +0400

    Настройка ридерикта при авторизации и постраничной навигации

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -2,9 +2,14 @@ import React, {useContext} from 'react';
 import {Navigate, Route, Routes} from "react-router-dom";
 import {publicRoutes, privateRoutes} from "../router/Router";
 import {AuthContext} from "../context";
+import Loader from "./UI/Loader/Loader";
 
 const AppRouter = () => {
-    const {isAuth, setIsAuth} = useContext(AuthContext)
+    const {isAuth, setLoading} = useContext(AuthContext)
+
+    if (setLoading){
+        return <Loader/>
+    }
 
     return (
         isAuth
