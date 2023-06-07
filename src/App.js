072550a3fa9c9c4aefd0cd7fc1f9b9f6c192bf37
Liss <les.karo@yandex.ru>
2023-06-07 12:32:31 +0400

    декомпозиция маршрутов

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
 import React from "react";
 import './styles/App.css'
-import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
-import About from "./pages/About";
-import Posts from "./pages/Posts";
+import {BrowserRouter} from "react-router-dom";
 import Navbar from "./components/UI/Navbar/Navbar";
-import Error from "./pages/Error";
+import AppRouter from "./components/AppRouter";
 
 function App() {
     return (
         <BrowserRouter>
             <Navbar/>
-            <Routes>
-                <Route
-                    path="/about"
-                    element={<About/>}
-                />
-                <Route
-                    path="/posts"
-                    element={<Posts/>}
-                />
-                <Route
-                    path="/error"
-                    element={<Error/>}
-                />
-                <Route
-                    path="*"
-                    element={<Navigate to='/error'/>}
-                />
-            </Routes>
+            <AppRouter/>
         </BrowserRouter>
     )
 }
