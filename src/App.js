b7c0bf9c65565de326e2d4354a49258ede1e3939
Liss <les.karo@yandex.ru>
2023-06-07 12:19:09 +0400

    Создание Navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from "react";
 import './styles/App.css'
-import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import About from "./pages/About";
 import Posts from "./pages/Posts";
+import Navbar from "./components/UI/Navbar/Navbar";
 
 function App() {
     return (
         <BrowserRouter>
-            <div className="navbar">
-                <div className="navbar__links">
-                    <Link to="/about">О сайте</Link>
-                    <Link to="/posts">Посты</Link>
-                </div>
-            </div>
-            <Route path="/about">
-                <About/>
-            </Route>
-            <Route path="/posts">
-                <Posts/>
-            </Route>
+            <Navbar/>
+            <Routes>
+                <Route
+                    path="/about"
+                    element={<About/>}
+                />
+                <Route
+                    path="/posts"
+                    element={<Posts/>}
+                />
+                <Route
+                    path="/*"
+                    element={<Navigate to="/error"/>}
+                />
+            </Routes>
         </BrowserRouter>
     )
 }
