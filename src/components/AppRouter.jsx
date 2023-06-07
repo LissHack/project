e88b87f13455ec7ff568aa414a48c1fb059dd389
Liss <les.karo@yandex.ru>
2023-06-07 14:47:22 +0400

    Улучшение навигации

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -4,30 +4,21 @@ import About from "../pages/About";
 import Posts from "../pages/Posts";
 import Error from "../pages/Error";
 import PostIdPage from "../pages/PostIdPage";
+import {routes} from "../router/Router";
 
 const AppRouter = () => {
     return (
         <Routes>
-            <Route
-                path="/about"
-                element={<About/>}
-            />
-            <Route
-                path="/posts"
-                element={<Posts/>}
-            />
-            <Route
-                path="/posts/:id"
-                element={<PostIdPage/>}
-            />
-
-            <Route
-                path="/error"
-                element={<Error/>}
-            />
+            {routes.map(route =>
+                <Route
+                    path={route.path}
+                    element={<route.component/>}
+                    exact={route.exact}
+                />
+            )}
             <Route
                 path="*"
-                element={<Navigate to='/error'/>}
+                element={<Navigate to='/posts'/>}
             />
         </Routes>
     );
